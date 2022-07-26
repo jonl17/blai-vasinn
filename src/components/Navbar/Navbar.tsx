e1bd110915ca9bcb362796529fb50bc0058d505a
Jón Gabríel Lorange <jonl17@ru.is>
2022-07-26 15:34:51 +0000

    basic setup ready for content

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,34 @@
+import Link from 'next/link'
 import React from 'react'
+import { IMenuItem } from '~/types'
 
-const Navbar = () => {
-  return <nav>menu navbar</nav>
+const MENU: Array<IMenuItem> = [
+  { slug: '/', label: 'Home' },
+  { slug: '/artists', label: 'Artists' },
+  { slug: '/tags', label: 'Tags' },
+  { slug: '/documents', label: 'Documents' },
+]
+
+interface INavbar {
+  items: Array<IMenuItem>
+}
+
+const Navbar = ({ items = MENU }: INavbar) => {
+  return (
+    <nav className="bg-black text-white w-full">
+      <ul className="flex justify-between h-24 max-w-lg mx-auto px-4">
+        {items.map((item, idx) => (
+          <Link key={idx} passHref href={item.slug}>
+            <a className="flex place-items-center">
+              <li>
+                <p className="underline">{item.label}</p>
+              </li>
+            </a>
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <div className="p-10 bg-black text-white h-screen">bob</div>
+  return (
+    <div className="p-10 bg-black text-white h-screen">
+      <h1>Blái vasinn</h1>
+    </div>
+  )
 }
 
 export default Home
