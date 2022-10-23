ce5ac1e7a62f2227e1445f9a5630b232ce2f4d04
lorange <jglorange@gmail.com>
2022-10-23 10:39:48 +0000

    hmm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
   return (
     <div className="p-5">
       <Text variant="medium">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
+        {`Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book. It has survived not only five
@@ -23,7 +23,7 @@ const Home: NextPage = () => {
         essentially unchanged. It was popularised in the 1960s with the release
         of Letraset sheets containing Lorem Ipsum passages, and more recently
         with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        of Lorem Ipsum.`}
       </Text>
     </div>
   )
