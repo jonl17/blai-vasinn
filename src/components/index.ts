c06a3626ef008adec30237cb0467aabc8762091a
lorange <jglorange@gmail.com>
2022-10-23 10:40:59 +0000

    rm sidebar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as Marquee } from './Marquee'
 export { default as SEO } from './SEO'
 export { default as PageLayout } from './PageLayout'
-export { default as Sidebar } from './Sidebar'
 export { default as Navbar } from './Navbar'
 export { default as Text } from './Text'
 export { default as ResizableTabs } from './ResizableTabs'
