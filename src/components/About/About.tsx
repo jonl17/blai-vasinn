3f26e8369b6a634177b331c020ffc920541f6f49
nonni <jglorange@gmail.com>
2022-12-11 21:48:36 +0000

    rm sanity stuff

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,18 +1,13 @@
-import { SanityType_about } from '@src/sanity-types'
 import RichText from '../RichText'
 import { Text } from '@src/components'
 
-type Props = {
-  about: SanityType_about
-}
-
-export default function About({ about }: Props) {
+export default function About() {
   return (
     <section className="text-center">
       <Text className="mb-2" as="h1" variant="large">
         um:
       </Text>
-      <RichText text={about?.text ?? []} />
+      {/* <RichText text={about?.text ?? []} /> */}
     </section>
   )
 }
